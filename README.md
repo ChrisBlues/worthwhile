6740b8fd8acabe35aa008777c10e41f0c4ff1437
ChrisBlues <chris.blues@free.fr>
2025-12-15 14:50:42 +0100

    Worthwhile avec 6 vidéos

---
+++ README.md
@@ -0,0 +1,7 @@
+# worthwhile
+30/10/2025 : Le point de départ est un tutoriel Youtube de Mr. Web Designer qui offre la possibilité d'utiliser cinq vidéos en page héro à l'aide de points de contrôle : cinq points sont visibles et chaque clic sur l'un d'eux fait apparaître une vidéo différente.
+Le problème est que, si la démonstration sur le tuto est évidente, en local, les points de contrôle ne sont pas opérationnels et ne déclenchent pas les vidéos. 
+
+31/10/2025 :
+- Worthwhile est terminé. Il va être déposé sur Github distant dans sa version définitive (sauf corrections envisageables), avec les cinq vidéos de moins de 50 Mo (limite imposée par Github).
+- Il reste un problème à revoir : la flèche de navigation droite de Swiper ne reste pas en place au premier clic, elle se décale sur l'image précédente et y reste. La flèche gauche est bien positionnée.
---
+++ js/script.js
@@ -0,0 +1,113 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+// Pour vidéos moins de 50 Mo
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+})
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+});
+
+// Pour vidéos moins de 50 Mo
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+// Initialize Swiper
+const swiper1 = new Swiper('.review-slider', {
+    spaceBetween: 20,
+    loop: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// Initialize Swiper brand section (curseur de marques publicitaires)
+const swiper2 = new Swiper('.brand-slider', {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction : false,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+
+        1200: {
+            slidesPerView: 5,
+        },
+    },
+});
+
+// Détection du bas de page pour les remerciements au concepteur
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY + window.innerHeight;
+  const pageHeight = document.documentElement.scrollHeight;
+  const footer = document.getElementById('thank-you-footer');
+
+  if (scrollPosition >= pageHeight - 50) {
+    footer.classList.add('visible');
+  } else {
+    footer.classList.remove('visible');
+  }
+});
